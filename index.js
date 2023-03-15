59cb01ff0eec860f0de980a97c6f2529507332be
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-03-15 14:05:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function PrependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
